62aeb4508af5e24275257a47200eedaefbe4b540
jberlar <jbernaus@mi.unc.edu.ar>
2025-04-05 17:15:06 -0300

    Agregado función en assembler - funcional

---
+++ myClient64.py
@@ -0,0 +1,8 @@
+from msl.loadlib import Client64
+
+# Extensión de la clase Client64 para cargar la biblioteca compartida
+class MyClient64(Client64):
+    def __init__(self):
+        super(MyClient64, self).__init__(module32='myServer32')
+    def ftoi_add1_64(self, value):
+        return self.request32('ftoi_add1_32', value)
