6d1d8879cb3a41d011db6b121b4416a1a7f83347
jberlar <jbernaus@mi.unc.edu.ar>
2025-04-11 16:17:27 -0300

    Moved files, corrected import paths, added readme

--- src/myServer32.py
+++ src/myServer32.py
@@ -1,10 +1,12 @@
 from msl.loadlib import Server32
 import ctypes
+import os
 
 # Extensión de la clase Server32 para cargar la biblioteca compartida
 class MyServer32(Server32):
     def __init__(self, host, port, **kwargs):
-        super(MyServer32, self).__init__('conversor_adder.so', 'cdll', host, port)
+        path_to_so = os.path.abspath(os.path.join(os.path.dirname(__file__), 'conversor_adder.so'))
+        super(MyServer32, self).__init__(path_to_so, 'cdll', host, port)
         self.lib.asm_float_to_int_add1.restype = ctypes.c_int
         self.lib.asm_float_to_int_add1.argtypes = [ctypes.c_float]
     def ftoi_add1_32(self, value):
